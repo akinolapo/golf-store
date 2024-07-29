55bdfc54599436e7f3acc794a3d6bee23f0846ca
akinolapo <akinolapo@gmail.com>
2024-07-29 14:22:50 -0400

    header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,161 @@
+'use client';
+
+import Image from "next/image";
+import React, { useState } from "react";
+import {
+  FaSearch,
+  FaRandom,
+  FaHeart,
+  FaShoppingCart,
+  FaBars,
+  FaUser,
+  FaTimes,
+} from "react-icons/fa";
+
+const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <header className="bg-gray-900 text-white">
+      <div className="container mx-auto flex justify-between items-center py-2 px-4">
+        <div className="flex items-center">
+          <span>Welcome you to Teknova store!</span>
+        </div>
+        <div className="flex items-center space-x-4">
+          <a href="#" className="hover:text-red-500 flex items-center space-x-1">
+            <FaUser />
+            <span>My Account</span>
+          </a>
+        </div>
+      </div>
+
+      <div className="bg-white text-black">
+        <div className="container mx-auto flex justify-between items-center py-4 px-4">
+          <div className="flex items-center space-x-4 lg:hidden">
+            <button className="hover:text-red-700" onClick={toggleMenu}>
+              <FaBars className="text-3xl" />
+            </button>
+          </div>
+          <div>
+            <a href="#">
+              <Image src="/logo-black.svg" width={140} height={32} alt="logo" />
+            </a>
+          </div>
+          <nav className="hidden lg:flex items-center space-x-8">
+            <a
+              href="#"
+              className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+            >
+              Home
+            </a>
+            <div className="relative group">
+              <a
+                href="#"
+                className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+              >
+                Shop
+              </a>
+              <div className="absolute hidden group-hover:block bg-white shadow-md">
+                {/* Add dropdown items here */}
+              </div>
+            </div>
+            <div className="relative group">
+              <a
+                href="#"
+                className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+              >
+                Accessories
+              </a>
+              <div className="absolute hidden group-hover:block bg-white shadow-md">
+                {/* Add dropdown items here */}
+              </div>
+            </div>
+            <a
+              href="#"
+              className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+            >
+              Specials
+            </a>
+            <a
+              href="#"
+              className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+            >
+              Contact Us
+            </a>
+            <a
+              href="#"
+              className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+            >
+              About Us
+            </a>
+            <div className="relative group">
+              <a
+                href="#"
+                className="relative font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
+              >
+                Blog
+                <span className="absolute -top-3 left-4 text-xs text-white bg-red-500 px-2 rounded-full">
+                  Hot
+                </span>
+              </a>
+              <div className="absolute hidden group-hover:block bg-white shadow-md">
+                {/* Add dropdown items here */}
+              </div>
+            </div>
+          </nav>
+          <div className="flex items-center space-x-4">
+            <button className="hover:text-red-700 hidden md:inline">
+              <FaSearch className="text-2xl" />
+            </button>
+            <button className="hover:text-red-700 hidden md:inline">
+              <FaRandom className="text-2xl" />
+            </button>
+            <button className="hover:text-red-700 hidden md:inline">
+              <FaHeart className="text-2xl" />
+            </button>
+            <div className="relative">
+              <button className="hover:text-red-700">
+                <FaShoppingCart className="text-3xl" />
+                <span className="absolute top-0 right-0 text-xs text-white bg-red-500 px-1 rounded-full">
+                  0
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Sliding Menu */}
+      {isMenuOpen && (
+        <div className="fixed inset-0 z-50 flex">
+          <div className="bg-white w-3/4 h-full shadow-lg py-4 relative">
+            <button className="absolute top-0 -right-10 text-white bg-black p-2" onClick={closeMenu}>
+              <FaTimes className="text-2xl" />
+            </button>
+            {/* Menu items go here */}
+            <nav className="flex flex-col">
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Home</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Shop</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Accessories</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Specials</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Contact Us</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">About Us</a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Blog</a>
+            </nav>
+          </div>
+          <div className="bg-black bg-opacity-50 w-1/4 h-full" onClick={closeMenu}></div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
