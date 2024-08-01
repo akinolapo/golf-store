f3650dace76cdc69c5a371b67a7541ff749fa5a7
akinolapo <akinolapo@gmail.com>
2024-07-31 21:35:44 -0400

    header cart items

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,20 +1,15 @@
 'use client';
 
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import {
-  FaSearch,
-  FaRandom,
-  FaHeart,
-  FaShoppingCart,
-  FaBars,
-  FaUser,
-  FaTimes,
-} from "react-icons/fa";
+import { FaSearch, FaRandom, FaHeart, FaShoppingCart, FaBars, FaUser, FaTimes, FaTrashAlt } from "react-icons/fa";
 import { MdKeyboardArrowDown } from "react-icons/md";
+import { useCart } from '@/app/context/CartContext'; // Import the useCart hook
 
 const Header: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { cartItems } = useCart(); // Get cart items from context
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -23,6 +18,9 @@ const Header: React.FC = () => {
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
+
+  // Get the total number of items in the cart
+  const totalItems = cartItems.reduce((count, item) => count + item.quantity, 0);
 
   return (
     <header className="bg-gray-900 text-white">
@@ -62,7 +60,7 @@ const Header: React.FC = () => {
                 href="/shop"
                 className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
               >
-                <span className="flex items-center">Shop <MdKeyboardArrowDown className="text-gray-400"/></span>
+                <span className="flex items-center">Shop <MdKeyboardArrowDown className="text-gray-400" /></span>
               </a>
               <div className="absolute hidden group-hover:block bg-white shadow-md w-[820px] left-0 top-full z-50">
                 <div className="container mx-auto p-4 pt-8">
@@ -122,7 +120,7 @@ const Header: React.FC = () => {
                 href="#"
                 className="font-semibold hover:text-red-700 hover:border-b-2 hover:border-red-500"
               >
-                <span className="flex items-center">Accessories <MdKeyboardArrowDown className="text-gray-400"/></span>
+                <span className="flex items-center">Accessories <MdKeyboardArrowDown className="text-gray-400" /></span>
               </a>
               <div className="absolute hidden group-hover:block bg-white shadow-md w-[820px] left-0 top-full z-50">
                 {/* Add dropdown items here */}
@@ -222,19 +220,19 @@ const Header: React.FC = () => {
               <FaHeart className="text-2xl" />
             </button>
             <div className="relative">
-              <button className="hover:text-red-700">
+              <Link href='/shop/cart' className="hover:text-red-700">
                 <FaShoppingCart className="text-3xl" />
                 <span className="absolute top-0 right-0 text-xs text-white bg-red-500 px-1 rounded-full">
-                  0
+                  {totalItems > 0 ? totalItems : 0}
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
 
       {/* Sliding Menu */}
-       {isMenuOpen && (
+      {isMenuOpen && (
         <div className="fixed inset-0 z-50 flex">
           <div className="bg-white w-3/4 h-full shadow-lg py-4 relative">
             <button className="absolute top-0 -right-10 text-white bg-black p-2" onClick={closeMenu}>
@@ -243,8 +241,12 @@ const Header: React.FC = () => {
             {/* Menu items go here */}
             <nav className="flex flex-col">
               <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Home</a>
-              <a href="/shop" className="text-black border-b-2 p-4 hover:text-red-700"><span className="flex items-center justify-between">Shop <MdKeyboardArrowDown className="text-gray-400 text-2xl"/></span></a>
-              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700"><span className="flex items-center justify-between">Accessories <MdKeyboardArrowDown className="text-gray-400 text-2xl"/></span></a>
+              <a href="/shop" className="text-black border-b-2 p-4 hover:text-red-700">
+                <span className="flex items-center justify-between">Shop <MdKeyboardArrowDown className="text-gray-400 text-2xl" /></span>
+              </a>
+              <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">
+                <span className="flex items-center justify-between">Accessories <MdKeyboardArrowDown className="text-gray-400 text-2xl" /></span>
+              </a>
               <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Specials</a>
               <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">Contact Us</a>
               <a href="#" className="text-black border-b-2 p-4 hover:text-red-700">About Us</a>
